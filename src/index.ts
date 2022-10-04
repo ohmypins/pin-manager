ebb6294488caac4792210e479b864e1179a8481d
ohmypinsdev <dev@ohmypins.app>
2022-10-04 01:48:12 -0300

    set error handling

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import { PIN_TAG, PIN_PORT, PIN_HOST } from './utils/constants';
 
 // Setup app
 const app: Application = express();
 
 // Setup middlewares
-app.use(express.static('public'));
+app.use('/', express.static('public'));
 
 // Setup routes
 app.use('/api', (_req: Request, res: Response) => res.json({ message: `Hello from ${PIN_TAG}` }));
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((_req: Request, res: Response, _next: NextFunction) => {
+  res.status(404).send('Page not found');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Internal error');
+});
 
 // Start server
 app.listen(PIN_PORT, () => console.log(`${PIN_TAG} - Pinned on http://${PIN_HOST}:${PIN_PORT}`));
