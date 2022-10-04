da4fd826a3e29b56b9c3b5370608b4c2e8823fa6
ohmypinsdev <dev@ohmypins.app>
2022-10-04 20:46:17 -0300

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,12 @@ app.use('/', express.static('public'));
 app.use('/api', (_req: Request, res: Response) => res.json({ message: `Hello from ${PIN_TAG}` }));
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((_req: Request, res: Response, _next: NextFunction) => {
-  res.status(404).send('Page not found');
-});
+app.use((_req: Request, res: Response, _next: NextFunction) =>
+  res.status(404).send('Page not found'),
+);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   console.error(err.stack);
   res.status(500).send('Internal error');
 });
